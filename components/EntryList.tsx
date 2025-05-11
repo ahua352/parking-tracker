66155cdad6d4e0e32b123c2c8cacd9d52ad3b6db
Amanda Huang <100510160+ahua352@users.noreply.github.com>
2025-05-11 21:42:26 +1200

    feat: Displayed message if no entries

--- components/EntryList.tsx
+++ components/EntryList.tsx
@@ -12,51 +12,59 @@ export function EntryList() {
 	return (
 		<ScrollView>
 			<View>
-				{entries.map((e, i) => {
-					// Duration in minutes
-					const duration = Math.round(
-						(e.dateEnd.getTime() - e.dateStart.getTime()) / (1000 * 60)
-					);
-					const hours = Math.floor(duration / 60);
-					const minutes = duration % 60;
+				{entries.length === 0 ? (
+					<ThemedText style={{ marginTop: 16 }}>No entries found.</ThemedText>
+				) : (
+					entries.map((e, i) => {
+						// Duration in minutes
+						const duration = Math.round(
+							(e.dateEnd.getTime() - e.dateStart.getTime()) / (1000 * 60)
+						);
+						const hours = Math.floor(duration / 60);
+						const minutes = duration % 60;
 
-					return (
-						<Pressable
-							key={i}
-							style={({ pressed }: { pressed: boolean }) => [
-								styles.row,
-								pressed && styles.rowPressed,
-							]}
-							onPress={() => {
-								console.log(`Entry ${e.id} pressed`);
-								router.push({
-									pathname: "/editEntry",
-									params: { id: e.id.toString() },
-								});
-							}}
-						>
-							<View style={styles.leftSection}>
-								<View style={styles.iconCircle}>
-									<FontAwesome name={iconMap[e.type]} size={24} color="pink" />
-								</View>
-								<View style={styles.textContainer}>
-									<ThemedText type="subtitle">{e.type}</ThemedText>
-									<ThemedText style={styles.dateStart}>
-										{e.dateStart.toLocaleString()}
-									</ThemedText>
-									<View style={styles.locationContainer}>
-										<ThemedText>{e.location}</ThemedText>
+						return (
+							<Pressable
+								key={i}
+								style={({ pressed }: { pressed: boolean }) => [
+									styles.row,
+									pressed && styles.rowPressed,
+								]}
+								onPress={() => {
+									console.log(`Entry ${e.id} pressed`);
+									router.push({
+										pathname: "/editEntry",
+										params: { id: e.id.toString() },
+									});
+								}}
+							>
+								<View style={styles.leftSection}>
+									<View style={styles.iconCircle}>
+										<FontAwesome
+											name={iconMap[e.type]}
+											size={24}
+											color="pink"
+										/>
+									</View>
+									<View style={styles.textContainer}>
+										<ThemedText type="subtitle">{e.type}</ThemedText>
+										<ThemedText style={styles.dateStart}>
+											{e.dateStart.toLocaleString()}
+										</ThemedText>
+										<View style={styles.locationContainer}>
+											<ThemedText>{e.location}</ThemedText>
+										</View>
 									</View>
 								</View>
-							</View>
-							<View style={styles.duration}>
-								<ThemedText>
-									{minutes === 0 ? `${hours} h` : `${hours} h ${minutes} m`}
-								</ThemedText>
-							</View>
-						</Pressable>
-					);
-				})}
+								<View style={styles.duration}>
+									<ThemedText>
+										{minutes === 0 ? `${hours} h` : `${hours} h ${minutes} m`}
+									</ThemedText>
+								</View>
+							</Pressable>
+						);
+					})
+				)}
 			</View>
 		</ScrollView>
 	);
