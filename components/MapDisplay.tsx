5d6c200f3c794ca0100323f43cf55976b713311d
Amanda Huang <100510160+ahua352@users.noreply.github.com>
2025-05-31 19:13:38 +1200

    feat: Added suggestions list

--- components/MapDisplay.tsx
+++ components/MapDisplay.tsx
@@ -1,4 +1,5 @@
 import {
+	FlatList,
 	Pressable,
 	StyleSheet,
 	TextInput,
@@ -11,9 +12,35 @@ import Constants from "expo-constants";
 import * as Location from "expo-location";
 import { useState } from "react";
 import { FontAwesome } from "@expo/vector-icons";
+import dummySuggestions from "../data/suggestions.json";
 
 export function MapDisplay() {
 	const [address, setAddress] = useState("");
+	type Suggestion = {
+		placePrediction: {
+			place: string;
+			placeId: string;
+			text: {
+				text: string;
+				matches?: {
+					endOffset: number;
+				}[];
+			};
+			structuredFormat?: {
+				mainText: {
+					text: string;
+					matches?: {
+						endOffset: number;
+					}[];
+				};
+				secondaryText: {
+					text: string;
+				};
+			};
+			types?: string[];
+		};
+	};
+	const [suggestions, setSuggestions] = useState<Suggestion[]>([]);
 	const colorScheme = useColorScheme();
 
 	// Fetch autocomplete suggestions from Google Places API
@@ -53,37 +80,43 @@ export function MapDisplay() {
 	const onPress = async () => {
 		console.log("Search button pressed");
 
-		let userLocation = undefined;
-		try {
-			const { status } = await Location.requestForegroundPermissionsAsync();
-			if (status === "granted") {
-				const location = await Location.getCurrentPositionAsync({});
-				userLocation = {
-					lat: location.coords.latitude,
-					lng: location.coords.longitude,
-				};
-			}
-		} catch (e) {
-			console.log("Location error:", e);
-		}
-		fetchAutocomplete(address, userLocation).then((suggestions) => {
-			console.log(JSON.stringify(suggestions, null, 2));
-		});
+		setSuggestions(dummySuggestions);
+
+		// let userLocation = undefined;
+		// try {
+		// 	const { status } = await Location.requestForegroundPermissionsAsync();
+		// 	if (status === "granted") {
+		// 		const location = await Location.getCurrentPositionAsync({});
+		// 		userLocation = {
+		// 			lat: location.coords.latitude,
+		// 			lng: location.coords.longitude,
+		// 		};
+		// 	}
+		// } catch (e) {
+		// 	console.log("Location error:", e);
+		// }
+		// fetchAutocomplete(address, userLocation).then((suggestions) => {
+		// 	setSuggestions(suggestions);
+		// 	console.log(JSON.stringify(suggestions, null, 2));
+		// });
 	};
 
 	const styles = StyleSheet.create({
+		container: { backgroundColor: "pink", padding: 8, gap: 8 },
 		mapContainer: {
 			flex: 1,
 		},
 		map: {
 			width: "100%",
 			height: 300,
+			backgroundColor: "lightgreen",
 		},
 		input: {
 			color: colorScheme === "dark" ? "#bfc7d5" : "black",
 			flex: 1,
 		},
 		inputContainer: {
+			height: 52,
 			flexDirection: "row",
 			alignItems: "center",
 			borderWidth: 1,
@@ -108,34 +141,75 @@ export function MapDisplay() {
 			borderColor: "black",
 			color: colorScheme === "dark" ? "#bfc7d5" : "black",
 		},
+		searchResults: {
+			backgroundColor: colorScheme === "dark" ? "#292d3e" : "white",
+			zIndex: 10,
+			borderBottomLeftRadius: 8,
+			borderBottomRightRadius: 8,
+			position: "absolute",
+			top: -6,
+			left: 0,
+			right: 0,
+		},
+		searchResultItem: {
+			padding: 8,
+			borderBottomWidth: 1,
+			borderBottomColor: colorScheme === "dark" ? "#444" : "#ccc",
+		},
+		searchResultText: {
+			fontSize: 14,
+		},
 	});
 	return (
-		<View style={{ backgroundColor: "pink", padding: 8, gap: 8 }}>
+		<View style={styles.container}>
 			{/* <TextInput
 				placeholder="Optional"
 				style={styles.inputExample}
 				placeholderTextColor="#72777f"
 			/> */}
 
-			<View style={styles.inputContainer}>
-				<TextInput
-					onChangeText={setAddress}
-					value={address}
-					placeholder="Enter location"
-					style={styles.input}
-					placeholderTextColor="#72777f"
-				/>
-				<Pressable onPress={onPress} style={styles.searchButton}>
-					<FontAwesome
-						name={"search"}
-						size={20}
-						color={colorScheme === "dark" ? "#bfc7d5" : "black"}
+			<View>
+				<View style={styles.inputContainer}>
+					<TextInput
+						onChangeText={setAddress}
+						value={address}
+						placeholder="Enter location"
+						style={styles.input}
+						placeholderTextColor="#72777f"
 					/>
-				</Pressable>
+					<Pressable onPress={onPress} style={styles.searchButton}>
+						<FontAwesome
+							name={"search"}
+							size={20}
+							color={colorScheme === "dark" ? "#bfc7d5" : "black"}
+						/>
+					</Pressable>
+				</View>
+
+				<View style={{ position: "relative" }}>
+					<FlatList
+						style={styles.searchResults}
+						data={suggestions}
+						keyExtractor={(_, idx) => idx.toString()}
+						renderItem={({ item }) => (
+							<Pressable
+								onPress={() => {
+									setAddress(item.placePrediction.text.text);
+									setSuggestions([]);
+								}}
+								style={styles.searchResultItem}
+							>
+								<ThemedText style={styles.searchResultText}>
+									{item.placePrediction.text.text}
+								</ThemedText>
+							</Pressable>
+						)}
+					/>
+				</View>
 			</View>
 
 			<View style={styles.mapContainer}>
-				<View style={[styles.map, { backgroundColor: "lightgreen" }]}></View>
+				<View style={styles.map}></View>
 				{/* <MapView style={styles.map} /> */}
 			</View>
 		</View>
